3d66f36a72673002b0ae65a8b68e8622b5de2d0e
drparsons <60318432+drparsons@users.noreply.github.com>
2021-08-21 14:09:30 -0400

    GUI refine

--- src/main/python/GUI/mainGUI.py
+++ src/main/python/GUI/mainGUI.py
@@ -2,7 +2,7 @@ import tkinter as tk
 
 import os, sys
 from tkinter import Menu, Scrollbar, font
-from tkinter.constants import END, RIGHT, Y
+from tkinter.constants import ANCHOR, CENTER, END, RIGHT, Y
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
@@ -14,7 +14,7 @@ from task import Task
 from type import Type
 
 
-  
+# ---- Create test variables
 type1 = Type("TestName1", "TestDescription1", "CHG001", "TestComment1")
 type2 = Type("TestName2", "TestDescription2", "CHG002", "TestComment2")
 task1 = Task(type1, "task1")
@@ -28,9 +28,12 @@ day0.addTask(task3)
 window = tk.Tk()
 #window.geometry("400x250")
 
+#give weight so it floats
+
 window.grid_rowconfigure(0, weight=1)
 window.grid_columnconfigure(0, weight=1)
 window.grid_columnconfigure(1, weight=1)
+
 
 #---------Table---------
 frame = tk.Frame(master=window, borderwidth=5)
@@ -39,6 +42,7 @@ frame.grid(row=0, column=0, sticky="nsew")
 #size
 TaskTypeWidth = 15
 TaskDescWidth = 25
+TaskTimeWidth = 10
 
 #Task Type
 e = tk.Entry(frame, width=TaskTypeWidth)
@@ -46,11 +50,20 @@ e.grid(row=0,column=0)
 e.insert(END, "Task Type")
 #Task Description
 f = tk.Entry(frame, width=TaskDescWidth)
-f.grid(row=0,column=TaskDescWidth)
+f.grid(row=0,column=1)
 f.insert(END, "Task Description")
 #TODO Start Time
+f = tk.Entry(frame, width=TaskTimeWidth)
+f.grid(row=0,column=2)
+f.insert(END, "Start Time")
 #TODO End Time
+f = tk.Entry(frame, width=TaskTimeWidth)
+f.grid(row=0,column=3)
+f.insert(END, "End Time")
 #TODO Duration
+f = tk.Entry(frame, width=TaskTimeWidth)
+f.grid(row=0,column=4)
+f.insert(END, "Duration")
 
 for itr, task in enumerate(day0.getTasks()):
     #Task Type
@@ -59,11 +72,20 @@ for itr, task in enumerate(day0.getTasks()):
     e.insert(END, str(task))
     #Task Description
     f = tk.Entry(frame, width=TaskDescWidth, fg='blue')
-    f.grid(row=itr + 1,column=TaskDescWidth)
+    f.grid(row=itr + 1,column=1)
     f.insert(END, str(task.getType()))
     #TODO Start Time
+    f = tk.Entry(frame, width=TaskTimeWidth)
+    f.grid(row=itr + 1,column=2)
+    f.insert(END, task1.get)
     #TODO End Time
+    f = tk.Entry(frame, width=TaskTimeWidth)
+    f.grid(row=itr + 1,column=3)
+    f.insert(END, "End Time")
     #TODO Duration
+    f = tk.Entry(frame, width=TaskTimeWidth)
+    f.grid(row=itr + 1,column=4)
+    f.insert(END, "Duration")
 
 
 #-------------Side Buttons---------------
@@ -74,24 +96,26 @@ button1 = tk.Button(
     text="New Type",
     width=15
 )
-button1.pack()
+button1.grid(row=0, column=0)
 button2 = tk.Button(
     master=frame2,
     text="End Day",
     width=15
 )
-button2.pack()
+button2.grid(row=1, column=0)
 
 #----------Bottom Buttons-----------
 #TODO doesn't work, should be on bottom
 frame3 = tk.Frame(master=window, border=5)
-frame3.grid(row=50, column=0, sticky="ew", columnspan=50)
+frame3.grid(row=2, column=0, sticky="ew", columnspan=2)
+frame3.grid_rowconfigure(0, weight=1)
+frame3.grid_columnconfigure(0, weight=1)
 button3 = tk.Button(
-    master=frame2,
+    master=frame3,
     text="New Task",
     width=15
 )
-button3.pack()
+button3.grid(row=0, column=0)#, columnspan=2)
 
 #-------------Menu Bar--------------
 menubar = Menu(window)
--- src/main/python/events/event.py
+++ src/main/python/events/event.py
@@ -10,20 +10,30 @@ class Event:
     def __init__(self):
         #start time
         self.start = time.time()
+        self.startString = time.strftime("%H:%M:%S %p")
         #End Time
         self.end = time.time()
+        self.endString = time.strftime("%H:%M:%S %p")
 
     #duration()
     def duration(self):
+        #Returns seconds
         return "%.2f" % (self.end - self.start)
 
     #getters and setters
     def setStart(self):
         self.start = time.time()
+        self.startString = time.strftime("%H:%M:%S %p")
 
     def setEnd(self):
         self.end = time.time()
+        self.endString = time.strftime("%H:%M:%S %p")
 
+    def getStart(self):
+        return self.startString
+    
+    def getEnd(self):
+        return self.endString
 
 
 if __name__ == "__main__":
@@ -32,4 +42,4 @@ if __name__ == "__main__":
     #task1.setStart()
     time.sleep(5)
     task1.setEnd()
-    print(task1.duration())
+    print("Start:",task1.getStart(),"\nEnd  :",task1.getEnd(),"\nDuration:",task1.duration(),"s")
